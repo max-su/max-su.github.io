5888b4515d8c9b4819c7b11c4bde4de087fc392a
Max Su <max.su7@gmail.com>
2017-06-30 10:52:38 -0500

    fix 404 routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="whomst" component={PageNotFound} />
+      <Route component={PageNotFound} />
     </Switch>
   );
 }
