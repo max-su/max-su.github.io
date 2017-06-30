24b6fbf85510781cc35212b55ca7057a8c0796ef
Max Su <max.su7@gmail.com>
2017-06-30 12:50:40 -0500

    add shared

---
+++ src/page_views/shared/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1> Whomst am I </h1>
+    </header>
+  );
+}
---
+++ src/page_views/shared/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <header>
+      <h1> Whomst am I </h1>
+    </header>
+  );
+}
