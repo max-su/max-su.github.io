fd814b1d98e5bcef8bd4c276b5020d4716bec29e
Max Su <max.su7@gmail.com>
2017-06-30 11:34:27 -0500

    add jquery as a dependency for materialize, add redirect to routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Home';
 import PageNotFound from './PageNotFound';
 
 export default function App() {
-  /* Switch
-   * Renders the first child <Route> or <Redirect> that matches the location.
-   */
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      { /* Redirects a user to /home */ }
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <Redirect to="/home" />;
+        }}
+      />
+      <Route path="/home" component={Home} />
       <Route component={PageNotFound} />
     </Switch>
   );
