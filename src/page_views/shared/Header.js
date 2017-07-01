d866183d04f661c528775bd10b2fc59f0d412012
Max Su <max.su7@gmail.com>
2017-06-30 23:44:37 -0500

    add scss

--- src/page_views/shared/Header.js
+++ src/page_views/shared/Header.js
@@ -6,7 +6,7 @@ import 'src/assets/styles/_header.scss';
 export default function Header() {
   return (
     <header className="center-align">
-      <img className="circle responsive-img" src={MaxSuJPG} alt="Max Su" />
+      <img className="circle responsive-img" id="header_icon" src={MaxSuJPG} alt="Max Su" />
       <h1> Whomst am I </h1>
     </header>
   );
