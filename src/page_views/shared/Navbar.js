61478e00ecbda3bfce3b04d36e70fc030a5ce5b6
Max Su <max.su7@gmail.com>
2017-07-01 17:42:49 -0500

    add navbar

--- src/page_views/shared/Navbar.js
+++ src/page_views/shared/Navbar.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import 'src/assets/styles/_navbar.scss';
 
+
 export default function Navbar() {
+  const navItems = (
+    <div>
+      <li>
+        <Link to={'/home'}>Home</Link>
+      </li>
+      <li>
+        <Link to={'/resume'}>Resume</Link>
+      </li>
+      <li>
+        <Link to={'/resources'}>Resources</Link>
+      </li>
+    </div>
+  );
   return (
     <nav className="emperor-gray-background">
       <a href="#!" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
       <div className="nav-wrapper center">
         <ul className="hide-on-med-and-down center">
-          {
-          // <li><a className="center" href="sass.html">Sass</a></li>
-          // <li><a className="center" href="badges.html">Components</a></li>
-          // <li><a className="center" href="collapsible.html">Javascript</a></li>
-          // <li><a className="center" href="mobile.html">Mobile</a></li>
-          }
+          {navItems}
+        </ul>
+        <ul className="side-nav" id="mobile-demo">
+          {navItems}
         </ul>
       </div>
     </nav>
   );
 }
+
