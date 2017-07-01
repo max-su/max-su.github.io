3a11134d60528209ad4b62738e9c04707cd69a7b
Max Su <max.su7@gmail.com>
2017-07-01 14:00:20 -0500

    add scss header, colors constants

--- src/page_views/shared/Header.js
+++ src/page_views/shared/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import MaxSuJPG from 'src/assets/images/max.jpg';
 
+import MaxSuJPG from 'src/assets/images/max.jpg';
+import { BackgroundClass } from 'src/assets/style_constants.js';
 import 'src/assets/styles/_header.scss';
 
 export default function Header() {
   return (
     <header className="center-align">
-      <img className="circle responsive-img" id="header_icon" src={MaxSuJPG} alt="Max Su" />
-      <h1> Whomst am I </h1>
+      <div className={BackgroundClass} id="header">
+        <img className="circle responsive-img" id="header_icon" src={MaxSuJPG} alt="Max Su" />
+        <h4 className="bitter-sweet-red-text">Max Su</h4>
+        <p className="aqua-background emperor-gray-text">Software Engineer</p>
+      </div>
     </header>
   );
 }
