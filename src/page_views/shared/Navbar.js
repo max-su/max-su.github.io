95799a208141095f925cbdd41bc4672aaa6cc729
Max Su <max.su7@gmail.com>
2017-07-01 15:58:01 -0500

    add placeholders for navbar, finish navbar scss

--- src/page_views/shared/Navbar.js
+++ src/page_views/shared/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import 'src/assets/styles/_navbar.scss';
 
 export default function Navbar() {
   return (
     <nav className="emperor-gray-background">
-      <h1> Whomst am I </h1>
+      <a href="#!" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+      <div className="nav-wrapper center">
+        <ul className="hide-on-med-and-down center">
+          {
+          // <li><a className="center" href="sass.html">Sass</a></li>
+          // <li><a className="center" href="badges.html">Components</a></li>
+          // <li><a className="center" href="collapsible.html">Javascript</a></li>
+          // <li><a className="center" href="mobile.html">Mobile</a></li>
+          }
+        </ul>
+      </div>
     </nav>
   );
 }
