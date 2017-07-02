4e4e068d0cb8cfb3829c3936579124322dbde8eb
Max Su <max.su7@gmail.com>
2017-07-02 13:59:54 -0500

    fix highliting on navbar

--- src/page_views/Home.js
+++ src/page_views/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from 'src/page_views/shared/Header';
-import Navbar from 'src/page_views/shared/Navbar';
+import NavBar from 'src/page_views/shared/Navbar';
 import Footer from 'src/page_views/shared/Footer';
 import Main from 'src/page_Views/Main';
 
@@ -9,7 +9,7 @@ export function Home() {
   return (
     <div>
       <Header />
-      <Navbar />
+      <NavBar />
       <Main />
       <Footer />
     </div>
---
+++ src/page_views/shared/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar, NavItem } from 'react-materialize';
+import { BackgroundClass } from 'src/assets/style_constants.js';
+import 'src/assets/styles/_navbar.scss';
+
+export default function NavBar() {
+  return (
+    <Navbar className={BackgroundClass} left>
+      <NavItem href={'/home'}>Home</NavItem>
+      <NavItem href={'/resume'}>Resume</NavItem>
+      <NavItem href={'/resources'}>Resources</NavItem>
+    </Navbar>
+  );
+}
