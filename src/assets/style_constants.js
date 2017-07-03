43b41c3354e9cfe80a3e98b15614a64199df5c9a
Max Su <max.su7@gmail.com>
2017-07-03 08:16:44 -0500

    added main footer styling and container

--- src/assets/style_constants.js
+++ src/assets/style_constants.js
@@ -5,6 +5,8 @@ export const HeaderText = 'bitter-sweet-red-text';
 export const HeaderButtonBackground = 'aqua-background';
 export const HeaderButtonText = 'emperor-gray-text';
 
+export const MainBackground = 'emperor-gray-background';
+
 export const FooterClassHeaderText = 'bitter-sweet-red-text';
 export const FooterClassText = 'egg-white-yellow-text';
 // export const ChipBackground = 'bitter-sweet-red-background';
--- src/page_views/Main.js
+++ src/page_views/Main.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-export function Main() {
+import { MainBackground } from 'src/assets/style_constants.js';
+import 'src/assets/styles/_main.scss';
+
+export default function Main() {
   return (
-    <main>
-      <p>Filler~</p>
+    <main className={MainBackground}>
+      <div className="container">
+        <div className="row">
+          <div className="col s12">
+            First row big kappa
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
-
-export default Main;
