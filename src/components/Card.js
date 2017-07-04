52e73a0fe28783a0ff80bfd4cd9c07253fd24603
Max Su <max.su7@gmail.com>
2017-07-04 10:02:47 -0500

    add css columns for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,6 @@ import { CardBackground,
 import 'src/assets/styles/_cards.scss';
 
 export default function Card(props) {
-  const columnClass = props.columnClass;
   const header = props.header;
   const body = props.body;
   let actionItem = props.actionItem;
@@ -20,27 +19,23 @@ export default function Card(props) {
     );
   }
   return (
-    <div className={`col ${columnClass}`}>
-      <div className={`card ${CardBackground}`}>
-        <div className={`card-content ${CardText}`}>
-          <span className={`card-title ${CardTextHeader}`}>
-            {header}
-          </span>
-          {body}
-        </div>
-        {actionItem}
+    <div className={`card ${CardBackground}`}>
+      <div className={`card-content ${CardText}`}>
+        <span className={`card-title ${CardTextHeader}`}>
+          {header}
+        </span>
+        {body}
       </div>
+      {actionItem}
     </div>
   );
 }
 
 Card.defaultProps = {
-  columnClass: 's12 m6',
   actionItem: null,
 };
 
 Card.propTypes = {
-  columnClass: PropTypes.string,
   header: PropTypes.string.isRequired,
   body: PropTypes.object.isRequired,
   actionItem: PropTypes.object,
--- src/page_views/Main.js
+++ src/page_views/Main.js
@@ -7,36 +7,64 @@ import { MainBackground, MainLinks } from 'src/assets/style_constants.js';
 import 'src/assets/styles/_main.scss';
 
 export default function Main() {
+  // const aboutMeBody = (
+  //   <p>
+  //     Hi! My name is Max and I&apos;m a junior studying Computer Science & Statistics at the
+  //     University of Illinois at Urbana-Champaign. I&apos;m currently working as a full stack
+  //     software engineering intern at&nbsp;
+  //     <a href="http://www.agrible.com/" className={MainLinks}>
+  //       <span role="img" aria-label="Chicken Emoji">
+  //         🐓
+  //       </span>
+  //       Agrible
+  //     </a>
+  //     &nbsp;writing React.js and Python.
+  //   </p>
+  // );
   const aboutMeBody = (
     <p>
       Hi! My name is Max and I&apos;m a junior studying Computer Science & Statistics at the
-      University of Illinois at Urbana-Champaign. I&apos;m currently working as a full stack
-      software engineering intern at&nbsp;
-      <a href="http://www.agrible.com/" className={MainLinks}>
-        <span role="img" aria-label="Chicken Emoji">
-          🐓
-        </span>
-        Agrible
-      </a>
-      &nbsp; writing React.js and Python.
+      University of Illinois at Urbana-Champaign.
+    </p>
+  );
+  const employmentBody = (
+    <p>
+      I&apos;m currently working as a full stack software engineering intern at&nbsp;
+        <a href="http://www.agrible.com/" className={MainLinks}>
+          <span role="img" aria-label="Chicken Emoji">
+            🐓
+          </span>
+          Agrible
+        </a>
+      &nbsp;writing React.js and Python.
     </p>
   );
   return (
     <main className={MainBackground}>
       <div className="container">
-        <div className="row">
-          <Card
-            header={'About me'}
-            body={aboutMeBody}
-          />
-          <Card
-            header={'About me'}
-            body={aboutMeBody}
-          />
-          <Card
-            header={'About me'}
-            body={aboutMeBody}
-          />
+        <div className="col s12 m6">
+          <div className="cards-container row">
+            <Card
+              header={'About me'}
+              body={aboutMeBody}
+            />
+            <Card
+              header={'Work'}
+              body={employmentBody}
+            />
+            <Card
+              header={'Work'}
+              body={employmentBody}
+            />
+            <Card
+              header={'Work'}
+              body={employmentBody}
+            />
+            <Card
+              header={'Work'}
+              body={employmentBody}
+            />
+          </div>
         </div>
       </div>
     </main>
